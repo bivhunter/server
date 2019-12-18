8381e384ce0e6bb420604bd64019c431b9610626
bivhunter_work <bivhunter@ukr.net>
2019-12-18 16:24:55 +0200

    go home

---
+++ app/public/js/TooltipComponent.js
@@ -0,0 +1,19 @@
+import {
+	Component
+}
+"./components/Component.js"
+
+
+export class TooltipComponent {
+	constructor(props) {
+		this.title = props.title;
+		this.status = props.status;
+		this.onInit();
+	}
+
+	onInit() {
+
+	}
+
+
+}
